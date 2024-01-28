08f06407b84735e1c82992e85c8085420c750eac
hacker3983 <mrgamer19554@gmail.com>
2024-01-27 22:23:01 -0500

    Update dict-tostring.c

--- dict-tostring.c
+++ dict-tostring.c
@@ -5,14 +5,14 @@ int main() {
 	dict_t* dict = dict_create(STRING, STRING, "Name", "John"),
 		*dict2 = dict_create(STRING, STRING, "Day", "Sunday");
 	dict_append(&dict, INTEGER, DICT, 10, dict2);
-//	string_t string = dict_tostring(dict);
-//	string_print(string); // equivalent to dict_print
-//	string_free(&string);
-	printf("dict2 pointer: %p\n", dict2);
+	string_t string = dict_tostring(dict);
+	string_print(string); // equivalent to dict_print
+	string_free(&string);
+	/*printf("dict2 pointer: %p\n", dict2);
 	printf("dict->right->dict pointer: %p\n", dict->right->dict);
 	dict_safe_free(&dict->right->dict);
 	dict_safe_free(&dict2);
-	printf("%p\n", dict2);
+	printf("%p\n", dict2);*/
 	dict_print(dict2);
 	dict_safe_free(&dict);
 	return 0;
